47063e43c1f682088e6bd8ddf0fa9aa706080e58
yenlee38 <ngocyen174308@gmail.com>
2021-05-19 22:08:35 +0700

    not create message

---
+++ src/main/java/com/apispring/api_spring/service/MessageService.java
@@ -0,0 +1,32 @@
+package com.apispring.api_spring.service;
+
+import com.apispring.api_spring.entity.Message;
+import com.apispring.api_spring.respository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MessageService {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    public Message createMessage(Message message){
+        return messageRepository.save(message);
+    }
+
+    public List<Message> getMessageBySenderAccountId(int accountId){
+        return messageRepository.findMessageBySenderAccountId(accountId);
+    }
+
+    public List<Message> getMessageByTwoAccountId(int senderId, int receiverId){
+        return messageRepository.findMessageByTwoAccountId(senderId, receiverId);
+    }
+
+    public Message getMessageById(int id){
+        return messageRepository.findById(id).orElse(null);
+    }
+}
